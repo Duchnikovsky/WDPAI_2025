deff0437ce247d482d117bc718c91bc7cb83477e
Duchnikovsky <filipduchnik@outlook.com>
2025-06-04 17:27:29 +0200

    feat: add comments to routing

--- index.php
+++ index.php
@@ -6,13 +6,16 @@ require 'Routing.php';
 $path = trim($_SERVER["REQUEST_URI"], '/');
 $path = parse_url($path, PHP_URL_PATH);
 
+//Pages routes
 Routing::get('index', 'DefaultController');
 Routing::get('dashboard', 'DefaultController');
 Routing::get('signup', 'DefaultController');
-Routing::get('logout', 'DefaultController');
 Routing::get('categories', 'DefaultController');
 Routing::get('bestsellers', 'DefaultController');
 Routing::get('management', 'DefaultController');
+
+//API routes
+Routing::get('logout', 'DefaultController');
 Routing::post("login", 'AuthController');
 Routing::post("register", 'AuthController');
 Routing::post('books', 'BookController');
